dbc6f3c11255ab52edc417ac0fcf8c60ae65a03c
masami <msihima0304@gmail.com>
2020-09-08 21:31:48 +0900

    feat: 🎸 settings

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -2282,12 +2282,8 @@ func getSettings(w http.ResponseWriter, r *http.Request) {
 	ress.PaymentServiceURL = getPaymentServiceURL()
 
 	categories := []Category{}
-
-	err := dbx.Select(&categories, "SELECT * FROM `categories`")
-	if err != nil {
-		log.Print(err)
-		outputErrorMsg(w, http.StatusInternalServerError, "db error")
-		return
+	for _, i := range categoryMap {
+		categories = append(categories, i)
 	}
 	ress.Categories = categories
 
